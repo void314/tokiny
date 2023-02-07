fad7e573d4bc0a521f8393adf48ab6ec28b9a794
void314 <artiemdrozdov@yandex.kz>
2023-02-07 12:01:44 +0600

    add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import Breadcrumbs from '@/components/breadcrumbs/breadcrumbs'
 import NavMenu from '@/components/nav-menu/nav-menu'
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
